ffc3e44fc1b76c13a635f22d0a3fd5af957263a1
Fabio Dela Bruna <fabio.bruna@agrotis.com>
2022-03-17 16:28:27 -0300

    First commit

---
+++ src/test/java/io/github/fabiodelabruna/springbootmicroservices/SpringBootMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fabiodelabruna.springbootmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

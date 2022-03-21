7f26a8a0f1e09eda2f25f95d6cebcc32ceb2434b
Fabio Dela Bruna <fabio.bruna@agrotis.com>
2022-03-20 21:54:01 -0300

    Creating gateway and service discovery

--- course/src/main/java/io/github/fabiodelabruna/ms/course/CourseApplication.java
+++ course/src/main/java/io/github/fabiodelabruna/ms/course/CourseApplication.java
@@ -3,11 +3,14 @@ package io.github.fabiodelabruna.ms.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan({"io.github.fabiodelabruna.ms.core.model"})
 @EnableJpaRepositories({"io.github.fabiodelabruna.ms.core.repository"})
+@ComponentScan("io.github.fabiodelabruna.ms")
 public class CourseApplication {
 
     public static void main(String[] args) {
--- discovery/src/test/java/io/github/fabiodelabruna/ms/discovery/DiscoveryApplicationTests.java
+++ discovery/src/test/java/io/github/fabiodelabruna/ms/discovery/DiscoveryApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.fabiodelabruna.ms.course;
+package io.github.fabiodelabruna.ms.discovery;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CourseApplicationTests {
+public class DiscoveryApplicationTests {
 
     @Test
     public void contextLoads() {
